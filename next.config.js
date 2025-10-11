5f929b00fe971da8aba7f356c47c78f80695eb50
daivya4 <daivyarocks@gmail.com>
2025-10-11 15:17:21 +0530

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  trailingSlash: true,
+  basePath: '',
+  assetPrefix: '',
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: { 
+    unoptimized: true 
+  },
 };
 
 module.exports = nextConfig;
